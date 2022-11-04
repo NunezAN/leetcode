2dbb77ac126b72862068f1dcbc01c8747dd16a46
NunezAN <alex101nunez@gmail.com>
2022-11-04 15:50:02 -0500

    completed #26

---
+++ 26.remove-duplicates-from-sorted-array.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=26 lang=javascript
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i=0;
+    for(let j=1;j<nums.length;j++){
+        if(nums[i]!==nums[j]){
+            i++;
+            nums[i]=nums[j];
+            console.log(nums);
+        }
+    }
+    return ++i;
+    
+};
+// @lc code=end
+
