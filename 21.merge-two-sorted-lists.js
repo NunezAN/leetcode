93f8c56cc3e465d2e5cf2c3ae062f6f3b2425f28
NunezAN <alex101nunez@gmail.com>
2022-11-04 11:26:23 -0500

    completed #21

--- 21.merge-two-sorted-lists.js
+++ 21.merge-two-sorted-lists.js
@@ -17,8 +17,22 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
-    
+var mergeTwoLists = function (list1, list2) {
+  var mergedHead = { val: -1, next: null },
+    sortedList = mergedHead;
+
+  while (list1 && list2) {
+    if (list1.val > list2.val) {
+      sortedList.next = list2;
+      list2 = list2.next;
+    } else {
+      sortedList.next = list1;
+      list1 = list1.next;
+    }
+    sortedList = sortedList.next;
+  }
+  sortedList.next = list1 || list2;
+
+  return mergedHead.next;
 };
 // @lc code=end
-
