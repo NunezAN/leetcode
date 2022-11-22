452a76922ef64c632fdf70e90c791248d1e0854d
NunezAN <alex101nunez@gmail.com>
2022-11-22 08:58:34 -0600

    completed #121

---
+++ 121.best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=121 lang=javascript
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    if (prices.length <= 1) {
+        return 0;
+    }
+
+    let maxprofit = 0;
+    let l=0;
+    let r=1;
+    // console.log("buy and sell prices",prices[l],prices[r]);
+    while (r<prices.length){
+        const profit = prices[r] - prices[l];
+        if(maxprofit<profit){
+            console.log("max profit",profit);
+            maxprofit = profit;
+        }
+        else if(profit<0){
+            l=r;
+            r=r+1;
+        }
+        else{
+            r=r+1;
+        }
+        // console.log("buy and sell prices",prices[l],prices[r]);
+    }
+    return maxprofit;
+};
+// @lc code=end
+
