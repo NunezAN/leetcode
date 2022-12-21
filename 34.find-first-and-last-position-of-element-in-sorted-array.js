e1287906187b8c09fcf02879aa1f9eb504d332ac
NunezAN <alex101nunez@gmail.com>
2022-12-20 18:23:57 -0600

    completed #34 w/ binary search

--- 34.find-first-and-last-position-of-element-in-sorted-array.js
+++ 34.find-first-and-last-position-of-element-in-sorted-array.js
@@ -11,23 +11,44 @@
  * @return {number[]}
  */
 var searchRange = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    let start;
-    if (nums[i] === target) {
-      start = i;
-      if (nums[start] != nums[start + 1] || start === nums.length) {
-        return [start, start];
+  const resultArr = [];
+  const leftMost = (nums, target) => {
+    console.log(nums);
+    let index = -1;
+    let l = 0;
+    let r = nums.length - 1;
+    while (l <= r) {
+      let mid = Math.floor((l + r) / 2);
+      if (target === nums[mid]) {
+        index = mid;
+        r = mid - 1;
+      } else if (target > nums[mid]) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
       }
-      let end = i;
-      while (end < nums.length) {
-        if (nums[end] !== target) {
-          break;
-        }
-        end++;
-      }
-      return [start, end - 1];
     }
-  }
-  return [-1, -1];
+    return index;
+  };
+  const rightMost = (nums, target) => {
+    let l = 0;
+    let r = nums.length - 1;
+    let index = -1;
+    while (l <= r) {
+      let mid = Math.floor((l + r) / 2);
+      if (target === nums[mid]) {
+        index = mid;
+        l = mid + 1;
+      } else if (target > nums[mid]) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+    return index;
+  };
+  resultArr.push(leftMost(nums, target));
+  resultArr.push(rightMost(nums, target));
+  return resultArr;
 };
 // @lc code=end
