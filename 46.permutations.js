d416582b008bb79285b103c354e3aae7bea9767c
NunezAN <alex101nunez@gmail.com>
2023-01-06 08:51:08 -0600

    completed #46

---
+++ 46.permutations.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=46 lang=javascript
+ *
+ * [46] Permutations
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+  const result = [];
+  const dfs = (i, nums) => {
+    if (i == nums.length) {
+      result.push(nums.slice());
+      return;
+    }
+    for (let j = i; j < nums.length; j++) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      dfs(i + 1, nums);
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  };
+  dfs(0, nums);
+  return result;
+};
+// @lc code=end
