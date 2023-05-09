e9a6e1c3fe9777dbf1f97e26c84ab07a382b5726
NunezAN <alex101nunez@gmail.com>
2023-05-09 13:27:24 -0500

    completed 26

--- 26.remove-duplicates-from-sorted-array.js
+++ 26.remove-duplicates-from-sorted-array.js
@@ -9,17 +9,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-    let i=0;
-    for(let j=1;j<nums.length;j++){
-        if(nums[i]!==nums[j]){
-            i++;
-            nums[i]=nums[j];
-            console.log(nums);
-        }
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+
+  let i = 0;
+
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
     }
-    return ++i;
-    
+  }
+
+  return i + 1;
 };
 // @lc code=end
-
