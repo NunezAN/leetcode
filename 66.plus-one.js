d4725e882c2a89527918d2a9fb01f2c657c9b72f
NunezAN <alex101nunez@gmail.com>
2023-05-11 09:38:43 -0500

    completed 66

--- 66.plus-one.js
+++ 66.plus-one.js
@@ -10,16 +10,18 @@
  * @return {number[]}
  */
 var plusOne = function (digits) {
-    for(let i =digits.length-1;i>=0;i--){
-        digits[i]++;
-        if(digits[i]>9){
-            digits[i]=0;
-        }
-        else{
-            return digits;
-        }
+  digits[digits.length - 1] += 1;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] > 9 && i !== 0) {
+      digits[i] = 0;
+      digits[i - 1] += 1;
+    } else if (i === 0 && digits[i] > 9) {
+      //console.log(digits[i], digits);
+      digits[i] = 0;
+      digits = [1, ...digits];
+     // console.log(digits[i], digits);
     }
-    digits.unshift(1)
-    return digits;
+  }
+  return digits;
 };
 // @lc code=end
