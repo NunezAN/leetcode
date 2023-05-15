e7974e8406ea41930469ba27e936f911b7deb334
NunezAN <alex101nunez@gmail.com>
2023-05-15 07:58:33 -0500

    completed 88

--- 88.merge-sorted-array.js
+++ 88.merge-sorted-array.js
@@ -13,19 +13,9 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-  let i = m + n - 1;
-  let p1 = m - 1;
-  let p2 = n - 1;
-  while (p2 >= 0) {
-    if (nums1[p1] > nums2[p2]) {
-      nums1[i] = nums1[p1];
-      p1--;
-    } else {
-      nums1[i] = nums2[p2];
-      p2--;
-    }
-    i--;
+  for (let i = m; i < m + n; i++) {
+    nums1[i] = nums2[i - m];
   }
-  //yep
+  nums1.sort((a, b) => a - b);
 };
 // @lc code=end
